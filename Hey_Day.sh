d1a9affa7a08b27e2d2a338d1b8f84f2a99cac14
sav0018 <sav0018@auburn.edu>
2024-10-15 19:34:33 -0500

    Hey Day Bonus

---
+++ Hey_Day.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Prompt the user to enter their name
+echo "Hello there! What's your name?"
+read name
+
+# Personalized greeting
+echo "Hey, $name! 👋  Happy Hey-Day! 🦅  War Eagle!"
+echo "I hope you're ready to have an awesome day at Auburn! 🚀"
+
